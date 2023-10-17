1175f3ca86dacef9563c6993bec95490bafdc450
Chrisonyi <128846708+Chrisonyi@users.noreply.github.com>
2023-10-17 09:04:50 +0100

    Delete 0x0B-python-input_output directory
    The above named repository should be deleted and there should be an access for me to create a new one.

--- 0x0B-python-input_output/4-from_json_string.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Contains a JSON-to-object function."""
-import json
-
-
-def from_json_string(my_str):
-    """Returns an object represented by a JSON string."""
-    return json.loads(my_str)
--- 0x0B-python-input_output/8-class_to_json.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Contains a class-to-JSON file."""
-
-
-def class_to_json(obj):
-    """Return the dictionary description of a simple data structure."""
-    return obj.__dict__
